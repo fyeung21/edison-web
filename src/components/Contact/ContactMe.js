95e454fc8a9e4ee4fc1415ecf0b10289c7bc448e
Fiona <fyeung21@gmail.com>
2020-07-09 10:58:23 -0700

    Style contact section

--- src/components/Contact/ContactMe.js
+++ src/components/Contact/ContactMe.js
@@ -1,20 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { ContactCont, ContactHeading, LinkCont, Email } from "./contactStyles";
-import { linkStyle } from "../Header/headerStyles";
+import { ContactCont, ContactHeading, LinkCont, BtnCont, ContactLink } from "./contactStyles";
+import { Mail, Linkedin } from "react-feather";
 
 const ContactMe = () => {
     return (
         <ContactCont>
             <ContactHeading>Let&#39;s chat!</ContactHeading>
             <LinkCont>
-                <Email
-                    href="mailto:koedison@gmail.com?subject=Let's chat!"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Email
-            </Email>
+                <BtnCont>
+                    <Mail color={"#41BAAE"} size={20} />
+                    <ContactLink
+                        href="mailto:koedison@gmail.com?subject=Let's chat!"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Email
+                </ContactLink>
+                </BtnCont>
+                <BtnCont>
+                    <Linkedin color={"#41BAAE"} size={20} />
+                    <ContactLink
+                        href="mailto:koedison@gmail.com?subject=Let's chat!"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        LinkedIn
+            </ContactLink>
+                </BtnCont>
             </LinkCont>
         </ContactCont>
     );
--- src/components/Contact/contactStyles.js
+++ src/components/Contact/contactStyles.js
@@ -11,12 +11,22 @@ export const ContactHeading = styled.h2`
     color: ${({ theme }) => theme.mainTxtColor};
 `
 export const LinkCont = styled.div`
-    margin: 3rem 0.15rem;
+    display: flex;
+    justify-content: space-evenly;
+    margin: 10rem auto;
 `
-export const Email = styled.a`
+export const ContactLink = styled.a`
     font-family: ${theme.bodyFont};
     font-size: ${theme.bodyFontSize};
     color: ${({ theme }) => theme.accentTxtColor};
     text-decoration: none;
-    margin: 2rem auto;
+    margin: 0 0.5rem;
+`
+export const BtnCont = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 2rem;
+    // border: 2px solid ${({ theme }) => theme.accentTxtColor};
+    // border-radius: 5px;
+    // padding: 1rem 3rem;
 `
