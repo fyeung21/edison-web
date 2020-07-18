107872083f9a92c0dfb82897d35d877a8a6b30fb
Fiona <fyeung21@gmail.com>
2020-07-18 10:03:37 -0700

    refactor: Remove PhotoLayout.js and apply to index for photography directly

--- src/pages/photo.js
+++ src/pages/photo.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { MainCont } from "../components/globalStyles";
-import PhotoLayout from "../components/Photo/PhotoLayout";
+import { PhotoCont } from "../components/Photo/photoStyles";
+import Portrait from "../components/Photo/Portrait";
+import Landscape from "../components/Photo/Landscape";
 
 const Photo = () => {
     return (
         <MainCont>
-            <PhotoLayout />
+            <PhotoCont>
+                <Portrait img={require("../assets/photography/edison_CR_002.jpg")} />
+                <Portrait img={require("../assets/photography/edison_CR_001.jpg")} />
+                <Landscape img={require("../assets/photography/edison_CR_003.JPG")} />
+                <Landscape img={require("../assets/photography/edison_CF_005f.jpg")} />
+                <Landscape img={require("../assets/photography/edison_CR_007.jpg")} />
+                <Landscape img={require("../assets/photography/coverpage2.JPG")} />
+                <Landscape img={require("../assets/photography/edison_CR_009.jpg")} />
+                <Landscape img={require("../assets/photography/edison_LE_005w.jpg")} />
+                <Landscape img={require("../assets/photography/edison_LE_004b.JPG")} />
+                <Landscape img={require("../assets/photography/edison_LE_002.JPG")} />
+                <Landscape img={require("../assets/photography/edison_LE_003.JPG")} />
+                <Landscape img={require("../assets/photography/edison_LE_001.JPG")} />
+                <Landscape img={require("../assets/photography/edison_LE_006.jpg")} />
+                <Landscape img={require("../assets/photography/edison_LE_007.jpg")} />
+                <Landscape img={require("../assets/photography/edison_MS_004.jpg")} />
+                <Landscape img={require("../assets/photography/edison_PT_abstract01b.jpg")} />
+                <Portrait img={require("../assets/photography/edison_MS_003.jpg")} />
+                <Landscape img={require("../assets/photography/edison_LK_001b.jpg")} />
+                <Landscape img={require("../assets/photography/edison_WS_honey01.JPG")} />
+                <Landscape img={require("../assets/photography/edison_WS_honey02.JPG")} />
+                <Portrait img={require("../assets/photography/edison_MS_002.JPG")} />
+            </PhotoCont>
         </MainCont>
     );
 }
