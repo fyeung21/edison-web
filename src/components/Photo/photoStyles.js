22150e7aa440567bacdac7b815f9d34733eb2f57
Fiona <fyeung21@gmail.com>
2020-07-26 16:25:57 -0700

    style: Increase margins between images and edit responsive margins

--- src/components/Photo/photoStyles.js
+++ src/components/Photo/photoStyles.js
@@ -1,26 +1,52 @@
 import styled from "styled-components";
+import { theme } from "../Theme";
+
+///////////////// Main Container for photo.js
 
 export const PhotoCont = styled.div`
-    margin: 4rem auto;
+    margin: 2.5rem auto 0;
+
+    @media (min-width: ${theme.tabletSize}) {
+        width: 650px;
+    }
+    @media (min-width: ${theme.desktopSize}) {
+        width: 1000px;
+    }
 `
 
-///////////////// PositionCont
+///////////////// PositionCont to get images to hug right
 
 export const PositionCont = styled.div`
     display: flex;
     justify-content: flex-end;
-    margin: 4rem 0;
+    margin: 0 0 4rem;
+
+    @media (min-width: ${theme.tabletSize}) {
+        margin: 0 0 10rem;
+    }
+    @media (min-width: ${theme.desktopSize}) {
+        margin: 0 0 15rem;
+    }
 `
 //////////////// Portrait/Landscape Cont
 
 export const PortraitCont = styled.div`
-    width: 645px;
-    height: 965px;
+    height: 565px;
+
+    @media (min-width: ${theme.skinnyWindowSize}) {
+        height: 620px;
+    }
+    @media (min-width: ${theme.tabletSize}) {
+        width: 645px;
+        height: 965px;
+    }
+    @media (min-width: ${theme.desktopSize}) {
+        height: 965px;
+    }
 `
 
 export const LandscapeCont = styled.div`
     width: 875px;
-    // height: 585px;
 `
 
 //////////////// Img
@@ -28,6 +54,6 @@ export const LandscapeCont = styled.div`
 export const PhotoImg = styled.img`
     height: 100%;
     width: 100%;
-    object-fit: cover;
+    object-fit: contain;
     filter: brightness(${({ theme }) => theme.imgBrightness});
 `
--- src/pages/photo.js
+++ src/pages/photo.js
@@ -6,31 +6,29 @@ import Landscape from "../components/Photo/Landscape";
 
 const Photo = () => {
     return (
-        <MainCont>
-            <PhotoCont>
-                <Portrait img={require("../assets/photography/edison_CR_002.jpg")} />
-                <Portrait img={require("../assets/photography/edison_CR_001.jpg")} />
-                <Landscape img={require("../assets/photography/edison_CR_003.JPG")} />
-                <Landscape img={require("../assets/photography/edison_CF_005f.jpg")} />
-                <Landscape img={require("../assets/photography/edison_CR_007.jpg")} />
-                <Landscape img={require("../assets/photography/coverpage2.JPG")} />
-                <Landscape img={require("../assets/photography/edison_CR_009.jpg")} />
-                <Landscape img={require("../assets/photography/edison_LE_005w.jpg")} />
-                <Landscape img={require("../assets/photography/edison_LE_004b.JPG")} />
-                <Landscape img={require("../assets/photography/edison_LE_002.JPG")} />
-                <Landscape img={require("../assets/photography/edison_LE_003.JPG")} />
-                <Landscape img={require("../assets/photography/edison_LE_001.JPG")} />
-                <Landscape img={require("../assets/photography/edison_LE_006.jpg")} />
-                <Landscape img={require("../assets/photography/edison_LE_007.jpg")} />
-                <Landscape img={require("../assets/photography/edison_MS_004.jpg")} />
-                <Landscape img={require("../assets/photography/edison_PT_abstract01b.jpg")} />
-                <Portrait img={require("../assets/photography/edison_MS_003.jpg")} />
-                <Landscape img={require("../assets/photography/edison_LK_001b.jpg")} />
-                <Landscape img={require("../assets/photography/edison_WS_honey01.JPG")} />
-                <Landscape img={require("../assets/photography/edison_WS_honey02.JPG")} />
-                <Portrait img={require("../assets/photography/edison_MS_002.JPG")} />
-            </PhotoCont>
-        </MainCont>
+        <PhotoCont>
+            <Portrait img={require("../assets/photography/edison_CR_002.jpg")} />
+            <Portrait img={require("../assets/photography/edison_CR_001.jpg")} />
+            <Landscape img={require("../assets/photography/edison_CR_003.JPG")} />
+            <Landscape img={require("../assets/photography/edison_CF_005f.jpg")} />
+            <Landscape img={require("../assets/photography/edison_CR_007.jpg")} />
+            <Landscape img={require("../assets/photography/coverpage2.JPG")} />
+            <Landscape img={require("../assets/photography/edison_CR_009.jpg")} />
+            <Landscape img={require("../assets/photography/edison_LE_005w.jpg")} />
+            <Landscape img={require("../assets/photography/edison_LE_004b.JPG")} />
+            <Landscape img={require("../assets/photography/edison_LE_002.JPG")} />
+            <Landscape img={require("../assets/photography/edison_LE_003.JPG")} />
+            <Landscape img={require("../assets/photography/edison_LE_001.JPG")} />
+            <Landscape img={require("../assets/photography/edison_LE_006.jpg")} />
+            <Landscape img={require("../assets/photography/edison_LE_007.jpg")} />
+            <Landscape img={require("../assets/photography/edison_MS_004.jpg")} />
+            <Landscape img={require("../assets/photography/edison_PT_abstract01b.jpg")} />
+            <Portrait img={require("../assets/photography/edison_MS_003.jpg")} />
+            <Landscape img={require("../assets/photography/edison_LK_001b.jpg")} />
+            <Landscape img={require("../assets/photography/edison_WS_honey01.JPG")} />
+            <Landscape img={require("../assets/photography/edison_WS_honey02.JPG")} />
+            <Portrait img={require("../assets/photography/edison_MS_002.JPG")} />
+        </PhotoCont>
     );
 }
 
