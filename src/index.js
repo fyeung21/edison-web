2e5ab80215f39fc18eb29c89af5e0341c8a270f5
Fiona <fyeung21@gmail.com>
2020-07-28 20:48:59 -0700

    feat: Apply scroll restoration to pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import './reset.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./reset.css";
+import "./index.css";
 import "./assets/fonts.css";
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+import ScrollToTop from "./components/hooks/scrollTop";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes />
     </BrowserRouter>
   );
