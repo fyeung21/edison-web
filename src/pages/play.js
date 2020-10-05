ab5772157f78638edd402d57f54e455204357f51
Fiona <fyeung21@gmail.com>
2020-10-05 03:36:06 -0700

    install: Add React modal image

--- src/pages/play.js
+++ src/pages/play.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { MainCont } from "../components/globalStyles";
 import Head from "../components/Head/Head";
+import ModalImage from "react-modal-image";
 
 const Play = () => {
     return (
         <MainCont>
             <Head title={"Play"} />
             <h1>test</h1>
+
+            <div style={{ width: 300 }}>
+                <ModalImage
+                    small={require("../assets/case-studies/concepts/ov_LPAbro1.jpg")}
+                    large={require("../assets/case-studies/concepts/ov_LPAbro1.jpg")}
+                    alt="Hello World!"
+                />
+            </div>
 
         </MainCont>
     );
