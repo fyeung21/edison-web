4cee99eda9411f139d4469a678c737d3dcd7581b
Fiona <fyeung21@gmail.com>
2020-10-06 16:29:53 -0700

    refactor: Remove unused code

--- src/pages/play.js
+++ src/pages/play.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { MainCont } from "../components/globalStyles";
 import { PlayCont, DisplayFlex } from "../components/Play/playStyles";
 import Head from "../components/Head/Head";
 import ImgModal from "../components/Play/ImgModal";
