6a255049d8c17caeb0bf8db96b52b62629468d6c
Fiona <fyeung21@gmail.com>
2020-10-05 19:26:24 -0700

    feat: Use different modal package to allow for customization

--- src/components/Play/ImgModal.js
+++ src/components/Play/ImgModal.js
@@ -1,17 +1,38 @@
 import React from "react";
-import ModalImage from "react-modal-image";
-import { ThumbnailCont } from "./playStyles";
+import { Image, ImgSmallCont, ImgExpandCont, OverlayCont } from "./playStyles";
+import { Modal, ModalWrapper, useModal } from "react-modal-wrap";
+
+const ModalCloseButton = ({ children }) => {
+    const { close } = useModal()
+    return <div onClick={close}>{children}</div>
+}
+const ModalOpenButton = ({ children }) => {
+    const { open } = useModal()
+    return <div onClick={open}>{children}</div>
+}
 
 const ImgModal = ({ imgURL, imgName }) => {
     return (
-        <ThumbnailCont>
-            <ModalImage
-                small={imgURL}
-                large={imgURL}
-                alt={imgName}
-                hideDownload={true}
-            />
-        </ThumbnailCont>
+        <>
+            <ModalWrapper style={{ position: "relative" }}>
+                <ModalOpenButton>
+                    <ImgSmallCont>
+                        <Image src={imgURL} alt={imgName} />
+                    </ImgSmallCont>
+                </ModalOpenButton>
+
+
+                <Modal overlay={true} lockScroll={true}>
+                    <OverlayCont>
+                        <ModalCloseButton>
+                            <ImgExpandCont>
+                                <Image src={imgURL} alt={imgName} />
+                            </ImgExpandCont>
+                        </ModalCloseButton>
+                    </OverlayCont>
+                </Modal>
+            </ModalWrapper>
+        </>
     );
 }
 
--- src/components/Play/playStyles.js
+++ src/components/Play/playStyles.js
@@ -12,7 +12,24 @@ export const DisplayFlex = styled.div`
     flex-wrap: wrap;
     justify-content: space-between;
 `
-export const ThumbnailCont = styled.div`
+export const ImgSmallCont = styled.div`
     width: 300px;
     height: 300px;
+    padding: 1rem 0;
+`
+export const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+export const ImgExpandCont = styled.div`
+    width: 800px;
+    height: 650px;
+    margin: 0 auto;
+    padding: 5rem 0;
+`
+export const OverlayCont = styled.div`
+    background-color: rgba(0, 0, 0, 0.8);
+    overflow: hidden;
+    z-index: 10;
 `
